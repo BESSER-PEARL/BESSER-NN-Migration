8deb47d6d44dcbf3c9f714fab4b365c40bb2fa2f
DaoudiNadia <daoudi.nadia.dn@gmail.com>
2026-09-04 12:04:58 +0200

    remove the hardcoded path

--- torch2tf/torch2tf_migrator.py
+++ torch2tf/torch2tf_migrator.py
@@ -11,7 +11,6 @@ Argument:
 
 import sys
 import os
-sys.path.insert(0, r'C:\Users\daoudi\projects\BESSER')
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from besser.generators.nn.tf.tf_code_generator import TFGenerator
 from ast_parser_pytorch import (
